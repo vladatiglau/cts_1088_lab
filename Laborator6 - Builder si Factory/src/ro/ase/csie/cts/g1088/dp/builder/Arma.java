c76de77c6cb194d8e185649e0f3ca2ae551aece5
vlada_tiglau <vlada.tiglau@gmail.com>
2021-04-01 12:40:59 +0300

    Implementare Builder (inainte de pauza)

---
+++ Laborator6 - Builder si Factory/src/ro/ase/csie/cts/g1088/dp/builder/Arma.java
@@ -0,0 +1,12 @@
+package ro.ase.csie.cts.g1088.dp.builder;
+
+public class Arma implements InterfataArma {
+
+	@Override
+	public void pocPoc() {
+		
+		System.out.println("Poc Poc -> -> -> -> ");
+		
+	}
+
+}
